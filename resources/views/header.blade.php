a997f1eb5ee310396c917644f513b6e48f541e92
andrew <adbalitsky@utexas.edu>
2019-02-19 21:26:05 -0500

    fix styles

---
+++ resources/views/header.blade.php
@@ -0,0 +1,22 @@
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Andrew Balitsky</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+
+    <!-- Styles -->
+    <style>
+        html, body
+        {
+          background-color: black;
+          color: white;
+          font-family: 'Nunito', sans-serif;
+          font-weight: 200;
+          height: 100vh;
+          margin: 0;
+        }
+    </style>
+</head>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,19 +1,6 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Andrew Balitsky</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
-
-        <!-- Styles -->
-        <style>
-
-        </style>
-    </head>
+    @include('header')
     <body>
         <div class="flex-center position-ref full-height">
 
