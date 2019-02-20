d26eff67dd22e5ab5ec6a7ae6b9a0f473c07d2b6
andrew <adbalitsky@utexas.edu>
2019-02-20 00:24:11 -0500

    disguise more env stuff
    although i suppose tracking_id shows up in the client anyway!

--- resources/views/partials/googleAnalytics.blade.php
+++ resources/views/partials/googleAnalytics.blade.php
@@ -4,13 +4,13 @@
 ?>
 @if(isProductionEnv() && $hasSecret)
 <!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-134837138-1"></script>
+<script async src="https://www.googletagmanager.com/gtag/js?id={{$googleSecret}}"></script>
 <script>
+  var ADBALITS_GA_SECRET = "{{ $googleSecret }}";
+
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
-
-  var ADBALITS_GA_SECRET = "{{ $googleSecret }}";
 
   gtag('config', ADBALITS_GA_SECRET);
 </script>
