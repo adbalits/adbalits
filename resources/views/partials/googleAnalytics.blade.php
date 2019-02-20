be531889084a8e889281f5a7eb3aa60853f690f5
andrew <adbalitsky@utexas.edu>
2019-02-20 00:20:53 -0500

    follow up on that todo
    whoopsie
    
    tracking id will be destroyed and rebuild for the .env file
    
    learn from my mistakes!

--- resources/views/partials/googleAnalytics.blade.php
+++ resources/views/partials/googleAnalytics.blade.php
@@ -1,7 +1,8 @@
 <?php
- $secrets = 'foo'; // #todo #cathat this probably needs to be secret/rehashed
+ $googleSecret = env('ADBALITS_GA_SECRET', null);
+ $hasSecret    = ! is_null($googleSecret);
 ?>
-@if(isProductionEnv())
+@if(isProductionEnv() && $hasSecret)
 <!-- Global site tag (gtag.js) - Google Analytics -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-134837138-1"></script>
 <script>
@@ -9,6 +10,8 @@
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
-  gtag('config', 'UA-134837138-1');
+  var ADBALITS_GA_SECRET = "{{ $googleSecret }}";
+
+  gtag('config', ADBALITS_GA_SECRET);
 </script>
 @endif
