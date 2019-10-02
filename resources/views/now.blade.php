6cd134680517227ac353bb06b0a34eb2599d04f3
Andrew Balitsky <adbalitsky@utexas.edu>
2019-10-02 09:51:14 -0400

    update now page

--- resources/views/now.blade.php
+++ resources/views/now.blade.php
@@ -6,9 +6,18 @@
 
 @section('content')
 <?php
-    $url_asbury = 'https://escapebrooklyn.com/guide-asbury-park/';
+    $url_asbury  = 'https://escapebrooklyn.com/guide-asbury-park/';
     $url_sesshin = 'http://coldmountainzen.org/events/sesshin/';
-    $url_now = 'https://nownownow.com/about';
+    $url_now     = 'https://nownownow.com/about';
+
+    $url_bro    = 'https://www.researchgate.net/profile/Philip_Balitsky';
+    $url_ghent  = 'https://www.ugent.be/en';
+    $url_phd    = 'https://www.researchgate.net/publication/334429158_A_Numerical_Investigation_of_the_Array_Effects_of_Wave_Energy_Converters_With_a_Realistic_Power_Take-off_System_Utilizing_a_Coupled_Model_Suite';
+
+    $url_brit   = 'https://www.google.com/search?biw=1440&bih=803&tbm=isch&sa=1&ei=uKiUXeDhJMSc1fAPt8606AM&q=point+de+la+torche&oq=point+de+la+torche&gs_l=img.3..0i30.1161.1581..1787...0.0..0.117.210.1j1......0....1..gws-wiz-img.TGUtI_whgYE&ved=0ahUKEwjgnevb2f3kAhVEThUIHTcnDT0Q4dUDCAY&uact=5';
+    $url_basq   = 'https://www.google.com/search?biw=1440&bih=803&tbm=isch&sa=1&ei=6aiUXYLwM5-W1fAPnMGQmA0&q=lafitenia&oq=lafitenia&gs_l=img.3..0l4j0i5i30j0i30l5.5734.5734..5957...0.0..0.100.100.0j1......0....1..gws-wiz-img.ARsgbQsKzBQ&ved=0ahUKEwjCiKnz2f3kAhUfSxUIHZwgBNMQ4dUDCAY&uact=5';
+    $url_hoss   = 'https://www.google.com/search?biw=1440&bih=803&tbm=isch&sa=1&ei=qqiUXYvqNrm61fAPwdGeuA4&q=hossegor&oq=hossegor&gs_l=img.3...94126.94253..94445...0.0..0.0.0.......0....1..gws-wiz-img.FlAmdzIVBp8&ved=0ahUKEwiL56bV2f3kAhU5XRUIHcGoB-cQ4dUDCAY&uact=5';
+    $url_quick  = 'https://www.hossegor.fr/en/discover/major-events/quiksilver-pro-france/';
 ?>
 <div class="books_container">
 <div class="books_backdrop">
@@ -23,23 +32,32 @@
 
     <h4>What I'm doing now</h4>
     <p class="ab_text">
-        Since I exited BeatGig a few months ago, for the first time in just over four years, I finally have the chance to do another sabbatical while staying by a good beach break
 
-         ( <a target="_blank" href="{{ $url_asbury }}">Asbury Park, NJ</a> ). 
+        For the first time in about five years (since we lived in the Outer Banks), <a href="{{ $url_bro }}">my brother</a> and I have ample time to spend together,
+
+        which for the last few weeks has taken the shape of an extended road/surf trip through Western Europe (mostly France).
+
+        He just finished his <a target="_blank" href="{{ $url_phd}}">PhD thesis</a> on marine engineering at <a href="{{ $url_ghent }}">Ghent University</a>, and I just left my company, which coincindentally means our paths began and ended at about the same pace.
+
+    </p>
+
+    <p>
+        So in early September we packed most of his belongings into a Ford Focus hatchback, tied a bike and boards around the shell, and rode off into the sunset from Ghent, Belgium for the coast of France.
+
+        After a quick stopover in Normandy, so far we've spent 10 days or so in <a target="_blank" href="{{ $url_brit }}">Brittany</a>, where we hiked for three days while the ocean was flat and then bunkered down for about a week while the ocean was blown-out and stormy
         
-        As is likely the case with other entrepreneurs who built a company from zero, through a seed stage, and on to stability, I was quite burned out.
-        
-        So I've spent the summer so far on meditation, reading, learning, and philosophizing.
+        before giving up and heading to Basque country near <a target="_blank" href="{{ $url_basq }}">Saint-Jean-de-Luz</a> for a few days of damn fine cliffside waves, weather, and camping.
 
-        It's the greatest pleasure to have free time again, and thankfully I'm in no rush to my next phase. 
+    </p>
 
-        In fact, just last night I came back from my first 
+    <p>
+        Right now we've pushed a little further north for more wilderness and beach breaks (Cap de L'Homy, St. Girons, <a target="_blank" href="{{ $url_hoss }}">Soorts-Hossegor</a>) but pretty soon we'll be joining friends for another round of Basque country,
 
-        <a target="_blank" href="{{ $url_sesshin }}">sesshin</a>.
+        and after a few weeks of that I have a ticket back to Newark. Maybe we'll stop by the <a target="_blank" href="{{ $url_quick }}">Quicksilver Pro</a> before we get there.
     </p>
 
     <p class="ab_text" style="color:white; color: grey;">
-        Updated: August 26
+        Updated: October 2
     </p>
 
     <hr class="ab_endpage" />
