3e6f4edf98e860f218a060a34739480deb81430e
adbalits <adbalits@adbalits.com>
2023-08-03 04:06:45 -0400

    upgrade to 10

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,18 @@
 <?php
 
-Route::get('/', 'Controller@welcome');
+use Illuminate\Support\Facades\Route;
 
-Route::get('about', 'Controller@about');
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
 
-Route::get('books', 'Controller@books');
-
-Route::get('books/{slug}', 'Controller@book');
-
-Route::get('now', 'Controller@now');
+Route::get('/', function () {
+    return view('welcome');
+});
