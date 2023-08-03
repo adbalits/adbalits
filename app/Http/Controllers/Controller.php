f93b2da93120ec402a3d541c5506757093741fb0
adbalits <adbalits@adbalits.com>
2023-08-03 04:11:12 -0400

    fix routing

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,12 +1,85 @@
-<?php
+<?php namespace App\Http\Controllers;
 
-namespace App\Http\Controllers;
-
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+
+use App\Book;
+use AB\Books\BookRepo;
 
 class Controller extends BaseController
 {
-    use AuthorizesRequests, ValidatesRequests;
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    /**
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function welcome()
+    {
+        return view('welcome');
+    }
+
+    /**
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function about()
+    {
+        return view('about', [
+            'title' => 'About',
+        ]);
+    }
+
+    /**
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function book($slug)
+    {
+        $book = Book::where('slug', $slug)->first();
+
+        if(is_null($book))
+        {
+            // not found
+            $books = approuter()->books()->route();
+            return redirect()->to($books);
+        }
+
+        $title = $book->fullTitle();
+
+        return view('book', [
+            'title' => $title,
+            'book'  => $book,
+        ]);
+    }
+
+    /**
+     *
+     * @param BookRepo $repo
+     *
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function books(BookRepo $repo)
+    {
+        $books = $repo->get();
+
+        return view('books', [
+            'title' => 'Books',
+            'books' => $books,
+        ]);
+    }
+
+    /**
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function now()
+    {
+        $previewStr = 'Over the last few years most of my friends have probably grown tired of hearing me talk about one day moving to New York City / Brooklyn...';
+
+        return view('now', [
+            'title' => 'Now',
+            'ogTitle' => "What I'm doing now",
+            'ogImg' => url('/images/nyc_tn.jpg'),
+            'ogDesc' => $previewStr,
+        ]);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Controller;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [Controller::class, 'welcome']);
+
+Route::get('about', [Controller::class, 'about']);
+
+Route::get('books', [Controller::class, 'books']);
+
+Route::get('books/{slug}', [Controller::class, 'book']);
+
+Route::get('now', [Controller::class, 'now']);
